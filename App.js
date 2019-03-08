4ca4aa48035157ea2919a091190af336c145b530
jlmolina <j.l.molina@cgiar.org>
2019-03-08 09:29:43 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {  
+    Text  
+  } from 'react-native';
+
+
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+     
+    );
+  }
+}
+
