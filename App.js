4d32d4f2cde4c5978bc8c90dae262b6e77070dfd
jlmolina <j.l.molina@cgiar.org>
2019-03-08 14:12:37 -0500

    creación de componentes - clase 12

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
-
 import React, {Component} from 'react';
 import {  
     Text  
   } from 'react-native';
 
+  import Home from './src/screens/containers/home';
+  import Header from './src/sections/components/header';
 
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-     
+      <Home>
+        <Header>
+            <Text/>
+        </Header>
+        <Text>buscador</Text>
+        <Text>categorias</Text>
+        <Text>sugerencia</Text>
+      </Home>     
     );
   }
 }
