dfa5d349ddc6e0234f59ceccf7c2c91f5d9b8999
jlmolina <j.l.molina@cgiar.org>
2019-03-11 17:17:09 -0500

    Instalando plugins de la comunidad - Clase 21

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {  
-    Text  
+    Text,
+    View,
+    StyleSheet 
   } from 'react-native';
 
   import Home from './src/screens/containers/home';
@@ -8,6 +10,7 @@ import {
   import SuggestionList from './src/videos/containers/suggestions-list';
   import API from './src/utils/api';
   import CategoryList from './src/videos/containers/category-list';
+  import Video from 'react-native-video';
 
 
 
@@ -31,7 +34,18 @@ export default class App extends Component<Props> {
   render() {
     return (
       <Home>
-        <Header/>            
+        <Header/>  
+        <View 
+          style= {styles.playerBox}>
+          <Video
+            source = {{
+              uri: 'https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4'
+            }}
+            style = {styles.player}
+            resizeMode = "contain"
+          />
+        </View>  
+        
         <Text>buscador</Text>
         <Text>categorias</Text>
         <CategoryList
@@ -44,4 +58,18 @@ export default class App extends Component<Props> {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  player:{
+    position: 'absolute',
+    left:0,
+    right:0,
+    bottom:0,
+    top:0,
+  },
+  playerBox :{
+    flex: 1,
+    height: 100,
+  }
+})
 
