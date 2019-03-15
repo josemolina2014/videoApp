0bf1e848f386f15a565b729654d632fd2453b8d9
jlmolina <j.l.molina@cgiar.org>
2019-03-15 09:43:23 -0500

    Clase 34: Scroll view y web view

--- src/videos/components/details.js
+++ src/videos/components/details.js
@@ -3,19 +3,60 @@ import {
     View,
     Text, 
     Image,
-    StyleSheet
+    StyleSheet,
+    WebView,
+    ScrollView    
 } from 'react-native';
+
+//import {WebView} from 'react-native-webview';
+
+const makeHTML = (id) =>{
+    return (`
+        <style>
+            .video{
+                position: relative;
+                padding-bottom: 56.25%
+            }
+            iframe {
+                position: absolute;
+                left: 0;
+                top:0;
+                right: 0;
+                bottom :0;
+                width: 100%;
+                height: 100%; 
+            }
+        </style>
+        <div class="video">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>        
+        ` )
+}
 
 function Details (props) {
     return (
-        <View>
-            <Image 
-                style= {styles.cover}
-                source={{uri: props.medium_cover_image}}
-            />
-            <Text>{props.title}</Text>
-            <Text>{props.description_full}</Text>            
-        </View>
+        <ScrollView>
+            <View style = {styles.top}>
+                <Text>{props.title}</Text>                
+            </View>
+            <View style={styles.bottom}>
+                <View style= {styles.details}>
+                    <Image 
+                        style= {styles.cover}
+                        source={{uri: props.medium_cover_image}}
+                    />
+                    <Text style= {styles.description}>{props.description_full}</Text>                  
+                </View>
+            </View>    
+            <View style={styles.trailer}>
+                 <WebView 
+                    source={{html : makeHTML(props.yt_trailer_code)}}
+                    originWhitelist={[`*`]}
+                    >
+                </WebView>
+            </View>                  
+           
+        </ScrollView>
     )
 }
 
@@ -23,9 +64,41 @@ const styles = StyleSheet.create({
     contain :{
 
     },
+    trailer:{
+        height : 200,
+        marginBottom: 10,
+    },
+    details :{
+        flexDirection: 'row',
+        marginBottom: 10,
+    },
     cover:{
         width:125,
         height: 190,
+    },
+    title :{
+        color : '#44546b',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    top:{
+        borderWidth: 1,
+        borderBottomColor: '#eaeaea',
+        padding:20,
+        backgroundColor: 'white',
+    },
+    bottom:{
+        padding : 20,
+        flex:1,
+    },
+    description :{
+        fontSize: 15,
+        lineHeight: 22.5,
+        color: '#4c4c4c',
+        marginLeft: 10,
+        flex:1,
+
     }
+
 })
 export default Details;
