babcdd8060036350651a29975ec4a69f799841b5
jlmolina <j.l.molina@cgiar.org>
2019-03-21 10:47:24 -0500

    react-navigation:
    Clase 15 - Añadiendo react navigation a Platzi Video

--- src/app-navigator-with-state.js
+++ src/app-navigator-with-state.js
@@ -6,37 +6,12 @@ import {
     reduxifyNavigator
 } from 'react-navigation-redux-helpers';
 
-//const addListener =  createReduxBoundAddListener('root');
 const AppNavigatorWithState = createReduxContainer(AppNavigator, 'root');
-const mapStateToProps = state => ({
-    state: state.navigation
-})
 
-/* class AppNavigatorWithState extends Component {
-
-    componentDidMount() {
-        initializeListeners('root', this.props.navigation);
-    }   
-
-    render (){
-
-        const navigation ={
-            dispatch: this.props.dispatch,
-            state: this.props.navigation,
-            // addListener : 
-        }
-        return  (
-            <AppNavigator 
-                navigation = {navigation}
-            />
-        )
-    }
-} */
-
-/* function mapStateToProps(state) {
+function mapStateToProps(state) {
     return {
-        navigation: state.navigation, //este campo deber ser un de los key declarados en el reducer
+        state: state.navigation
     }
+}
 
-} */
 export default connect(mapStateToProps)(AppNavigatorWithState);
